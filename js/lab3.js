1c516d8f0a859fd500033fa7b192bafef8bec454
mohdfahoum <mohd.fahoum@gmail.com>
2020-07-07 16:37:42 +0300

    lab3-2

--- js/lab3.js
+++ js/lab3.js
@@ -10,15 +10,15 @@ var guessArray = new Array();
 guessArray.push(guess); // pushes the answers inside the array
 
 while (tries > 0) { //used the while to keep tries till reach 4 times
-   guess = prompt('You have ' + tries + ' guesses left.');
+   guess = prompt(hint + 'You have ' + tries + ' guesses left.');
   if (!guess) prompt("you have to try a number");
   guess = Number(guess);
   if (guess == x) {
     alert("Nice job! You guessed the correct number! It took you " + guessArray.length + " tries!");
     tries = 0;
   } else {
-    if (guess < x) hint += ' Too small!';
-    if (guess > x) hint += ' Too big!';
+    if (guess < x) hint = ' Too small!';
+    if (guess > x) hint = ' Too big!';
     tries = tries - 1;
   }
 }
